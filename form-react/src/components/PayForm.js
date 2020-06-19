e187bf450da7b9c8fdf10e182fed5357e11d33f9
rachelvo <rach0727@stud.kea.dk>
2020-06-19 12:22:29 +0200

    remove unused components

--- form-react/src/components/PayForm.js
+++ form-react/src/components/PayForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { Heroku } from "../modules/Heroku";
 import { useHistory } from "react-router-dom";
 import NumberFormat from "react-number-format";
