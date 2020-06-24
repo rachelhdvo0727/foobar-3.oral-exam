8a34a26f995ad3a7d7c1dc177663c7d57a91c198
rachelvo <rach0727@stud.kea.dk>
2020-06-24 15:02:00 +0200

    deleted unused console.log

--- form-react/src/components/Mug.js
+++ form-react/src/components/Mug.js
@@ -4,7 +4,6 @@ import styles from "./Mug.modules.css";
 export default function Mug(props) {
   let imgName = props.name.toLowerCase();
   let label = imgName.split(" ").join("");
-  console.log(props.name);
 
   return (
     <svg
